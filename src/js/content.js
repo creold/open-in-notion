ec4cb1d41bdfd50b543b96b34fbf6f3c2ea98b47
Sergey Osokin <reg@sergosokin.ru>
2021-01-15 17:19:43 +0500

    exclude reserved links
    Fixed an issue where the extension redirected branded links. For example, notion.so/about

--- src/js/content.js
+++ src/js/content.js
@@ -1,6 +1,11 @@
 var storage = chrome.storage.local;
 var loc = document.location;
 var tabUrl = loc.href;
+// Reserved sections of the Notion website
+var reservedList = ["signup", "login", "careers", "pricing", "customers", "guides", "enterprise", 
+                    "mobile", "desktop", "web-clipper", "product", "wikis", "projects", "notes",
+                    "teams", "remote", "personal", "startups", "students", "educators", "evernote",
+                    "confluence", "api-beta", "about", "tools-and-craft"];
 
 // Get extension options
 storage.get(["OINStatus", "OINCloseTab", "OINCloseTime", "OINWorkspaces"], function (data) {
@@ -9,16 +14,25 @@ storage.get(["OINStatus", "OINCloseTab", "OINCloseTime", "OINWorkspaces"], funct
   let time = data.OINCloseTime;
   let workspaces = data.OINWorkspaces;
   
+  // Collect a list of reserved words
+  let reservedRegex = reservedList.join("|");
+
+  // Collect a list of user's workspaces
   let workspacesList = (workspaces || '').trim().split(/[ ,]+/);
   let workspacesRegex = "(" + workspacesList.join("|") + ")";
   
+  // Accumulated expression
+  let reservedExpression = "(https:\/\/www\.notion\.so\/)(?!" + reservedRegex + ").+";
   let expression = "((https:\/\/www\.notion\.so\/)(native\/)?" + workspacesRegex + ").+";
   
+  var notionReservedRegex = new RegExp(reservedExpression);
+  let reservedMatch = notionReservedRegex.exec(tabUrl);
+
   var notionRegex = new RegExp(expression);
   let match = notionRegex.exec(tabUrl);
-
+  
   // Get extension status
-    if (statusExt || statusExt == undefined) {
+    if ((statusExt || statusExt == undefined) && reservedMatch != null) {
       if (match != null) {
         if (tabUrl.indexOf("/native/") == -1) {
           loc.replace(tabUrl.replace(match[2], match[2] + "native/"));
