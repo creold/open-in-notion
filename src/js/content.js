f8109e1b49622ca20123bd05fb3a1374ee051878
danielmizrachi <daniel@curvestone.io>
2021-10-01 12:26:22 +0100

    Add "help" to reserved notion.so endpoints

--- src/js/content.js
+++ src/js/content.js
@@ -5,7 +5,7 @@ var tabUrl = loc.href;
 var reservedList = ["signup", "login", "careers", "pricing", "customers", "guides", "enterprise", 
                     "mobile", "desktop", "web-clipper", "product", "wikis", "projects", "notes",
                     "teams", "remote", "personal", "startups", "students", "educators", "evernote",
-                    "confluence", "api-beta", "about", "tools-and-craft", "unsubscribe"];
+                    "confluence", "api-beta", "about", "tools-and-craft", "unsubscribe", "help"];
 
 // Get extension options
 storage.get(["OINStatus", "OINCloseTab", "OINCloseTime", "OINWorkspaces"], function (data) {
